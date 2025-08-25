94616dbec0dfca95981b4f54fb11f7f3392057f2
Park Joon Seo <junseu94@naver.com>
2025-08-25 15:17:01 +0900

    cafe

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import "./App.scss";
-import data from "./assets/data";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart";
 
 function App() {
-  const [menu, setMenu] = useState(data.menu);
   const [cart, setCart] = useState([]);
   console.log(cart);
 
@@ -18,11 +16,11 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={<Menu menu={menu} cart={cart} setCart={setCart} />}
+            element={<Menu cart={cart} setCart={setCart} />}
           />
           <Route
             path="/cart"
-            element={<Cart menu={menu} cart={cart} setCart={setCart} />}
+            element={<Cart cart={cart} setCart={setCart} />}
           />
         </Routes>
       </main>
